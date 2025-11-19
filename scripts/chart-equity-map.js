8e99449c66dde91f3dd80907db6b2ae25b726eb3
Justin Lee <ju3tinlee@gmail.com>
2025-11-19 12:33:34 -0800

    Removed unecessary line

--- scripts/chart-equity-map.js
+++ scripts/chart-equity-map.js
@@ -1,5 +1,3 @@
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
-
 export async function drawEquityMap() {
     const container = document.getElementById("equity-map");
     if (!container) return;
