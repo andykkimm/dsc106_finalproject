ea4e9fe3ac4ac816ea7adae6e93873521358dad8
Justin Lee <ju3tinlee@gmail.com>
2025-11-19 12:32:52 -0800

    Executes all code in main.js now

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
 import { drawEquityMap } from "./chart-equity-map.js";
 import { drawStationMap } from "./chart-station-map.js";
-import { drawHeatmap } from "./heatmap.js";
+import { drawHeatmap } from "./chart-heatmap.js";
 import { drawDurationChart } from "./chart-duration.js";
+import { drawStaticFlowMap } from "./chart-static-flow-map.js";
+import { drawHourly } from "./chart-hourly.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
   drawStationMap();
   drawHeatmap();
   drawEquityMap();
   drawDurationChart();
+  drawStaticFlowMap();
+  drawHourly();
 });
